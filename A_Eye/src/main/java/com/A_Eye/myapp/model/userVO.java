08134ec3ed48077eb147986e237f29be6b794214
KIMGUUNI <kdd1953@naver.com>
2024-01-24 15:38:02 +0900

    VO 수정

--- A_Eye/src/main/java/com/A_Eye/myapp/model/userVO.java
+++ A_Eye/src/main/java/com/A_Eye/myapp/model/userVO.java
@@ -9,8 +9,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class userVO {
    
-   private String name;
-   private String password;
-   private String email;
-   private String ssn;
+	 // 사용자 번호 
+    private Long userIdx;
+
+    // 사용자 이메일 
+    private String userEmail;
+
+    // 사용자 비밀번호 
+    private String userPw;
+
+    // 사용자 가입날자 
+    private String userSignDate;
+
+    // 사용자 position 
+    private String userPosition;
 }
