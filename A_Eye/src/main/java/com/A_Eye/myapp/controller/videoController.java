0278d369df79c636410801bf0e7c15ed7ce85e60
KIMGUUNI <kdd1953@naver.com>
2024-02-01 17:22:23 +0900

    axios 커밋

--- A_Eye/src/main/java/com/A_Eye/myapp/controller/videoController.java
+++ A_Eye/src/main/java/com/A_Eye/myapp/controller/videoController.java
@@ -2,7 +2,9 @@ package com.A_Eye.myapp.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class videoController {
+	
 	
 }
---
+++ A_Eye/src/main/java/com/A_Eye/myapp/mapper/adMapper.java
@@ -0,0 +1,15 @@
+package com.A_Eye.myapp.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.A_Eye.myapp.model.adVO;
+import com.A_Eye.myapp.model.userVO;
+	
+@Mapper
+public interface adMapper {
+
+	List<adVO> getUserAds(userVO vo);
+		
+}
